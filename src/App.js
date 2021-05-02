b4a49f5d99a0750711fa2b0b28e00d6d209389fb
YavatmalCares <yavatmalcovid@gmail.com>
2021-05-02 21:10:10 +0530

    Updated Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+import {  ButtonToolbar, Button,Card,Navbar,Nav } from 'react-bootstrap'
+import plasma from './plasma.gif';
 
 function App() {
+  const [show,setShow]=useState(false);
+  const [showR,setShowR]=useState(false);
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar className="color-navb" variant="dark">
+    <Navbar.Brand className="navbar">Yavatmal-Cares</Navbar.Brand>
+    </Navbar>
+    
+
+      <Card className="text-center">
+      <Card.Img variant="top" src={plasma} alt="test" />
+        <Card.Body>
+          <Card.Title>Plasma Donation and Request</Card.Title>
+          <Card.Text>Fill one of these forms.</Card.Text>
+          <Card.Text>Our team will match relevant donors and patients and put them in touch with each other.</Card.Text>
+          <Card.Text> We are a group of volunteers helping our city get over this COVID-19 crisis.</Card.Text>
+
+          <ButtonToolbar className="custom-btn-toolbar">
+            <Button onClick={()=>{setShow(!show); setShowR(false)}}>{show?'Close Donation Form':'Donate Plasma'}</Button>
+            <Button onClick={()=>{setShowR(!showR); setShow(false)}}>{showR?'Close Request Form':'Request Plasma'}</Button>
+          </ButtonToolbar>
+        </Card.Body>
+      </Card>
+     
+        <div className="Box">
+              { 
+              show?
+              <iframe class="airtable-embed" src="https://airtable.com/embed/shr6Pf2Dc3odgIVpc?backgroundColor=yellow" 
+              frameborder="0" onmousewheel="" width="100%" height="533" 
+              styles="background: transparent; border: 1px solid #ccc;">
+              </iframe>
+              :null}
+              {
+              showR?
+              <iframe class="airtable-embed" src="https://airtable.com/embed/shrlPtj561qqfHLok?backgroundColor=yellow" 
+              frameborder="0" onmousewheel="" width="100%" height="533" 
+              styles="background: transparent; border: 1px solid #ccc;"/>:null
+              } 
+        </div>
+
+        <Navbar className="justify-content-end" fixed="bottom">
+          <Navbar.Text >
+            Developer: <a href="https://github.com/AdityaChondke">Aditya Chondke</a>
+          </Navbar.Text>
+      </Navbar>
+    
     </div>
   );
 }
